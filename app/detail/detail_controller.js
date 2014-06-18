33db664c796b09276ca33dcac0d9f2b48550c220
Jorge Cruz <jorge@tvplus.com>
2014-06-18 13:24:21 -0700

    add more sample facts

--- app/detail/detail_controller.js
+++ app/detail/detail_controller.js
@@ -45,6 +45,58 @@
               "p": { "type": "uri" , "value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" },
               "predicate_label": { "type": "literal" , "value": "type" },
               "o": { "type": "uri" , "value": "http://dbpedia.org/class/yago/Rapper110507482" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/occupation" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "occupation" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/Rapping" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "Rapping" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "75.50723" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/occupation" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "occupation" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/Actor" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "Actor" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "303.58078" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/birthDate" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "birth date" } ,
+              "o": { "datatype": "http://www.w3.org/2001/XMLSchema#date" , "type": "typed-literal" , "value": "1971-06-15+02:00" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/deathDate" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "death date" } ,
+              "o": { "datatype": "http://www.w3.org/2001/XMLSchema#date" , "type": "typed-literal" , "value": "1996-09-12+02:00" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/birthPlace" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "birth place" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/New_York_City" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "New York City" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "813.0256" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/birthPlace" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "birth place" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/New_York" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "New York" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "625.0301" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/deathPlace" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "death place" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/Las_Vegas" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "Las Vegas" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "57.24966" }
+            },
+            {
+              "p": { "type": "uri" , "value": "http://dbpedia.org/ontology/deathPlace" } ,
+              "predicate_label": { "type": "literal" , "xml:lang": "en" , "value": "death place" } ,
+              "o": { "type": "uri" , "value": "http://dbpedia.org/resource/Nevada" } ,
+              "object_label": { "type": "literal" , "xml:lang": "en" , "value": "Nevada" } ,
+              "rank": { "datatype": "http://www.w3.org/2001/XMLSchema#float" , "type": "typed-literal" , "value": "115.70988" }
             }
           ]
         }
