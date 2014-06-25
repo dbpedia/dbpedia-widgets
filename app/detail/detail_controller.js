b7f517139ca6bd83619c7ff312acbe4ffb5c23ec
Jorge Cruz <jorge@tvplus.com>
2014-06-25 02:33:15 -0700

    format json

--- app/detail/detail_controller.js
+++ app/detail/detail_controller.js
@@ -24,112 +24,115 @@
 				'facts': [
 					{
 						'id': 'bio',
-						'facts': [{
-							'predicate': {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/ontology/birthDate'
-							},
-							'predicate_label': {
-								'type': 'literal',
-								'xml:lang': 'en',
-								'value': 'Birth date'
-							},
-							'objects': [{
-								'type': 'typed-literal',
-								'datatype': 'http://www.w3.org/2001/XMLSchema#date',
-								'value': '1971-06-15+02:00'
-							}]
-						},
-						{
-							'predicate': {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/ontology/birthPlace'
-							},
-							'predicate_label': {
-								'type': 'literal',
-								'xml:lang': 'en',
-								'value': 'Birth place'
-							},
-							'objects': [{
-								'type': 'uri',
-								'value': 'http://dbpedia.org/resource/New_York_City',
-								'object_label': {
+						'facts': [
+							{
+								'predicate': {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/ontology/birthDate'
+								},
+								'predicate_label': {
 									'type': 'literal',
 									'xml:lang': 'en',
-									'value': 'New York City'
-								}
-							}]
-						},
-						{
-							'predicate': {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/ontology/deathDate'
+									'value': 'Birth date'
+								},
+								'objects': [{
+									'type': 'typed-literal',
+									'datatype': 'http://www.w3.org/2001/XMLSchema#date',
+									'value': '1971-06-15+02:00'
+								}]
 							},
-							'predicate_label': {
-								'type': 'literal',
-								'xml:lang': 'en',
-								'value': 'Death date'
-							},
-							'objects': [{
-								'type': 'typed-literal',
-								'datatype': 'http://www.w3.org/2001/XMLSchema#date',
-								'value': '1996-09-12+02:00'
-							}]
-						},
-						{
-							'predicate': {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/ontology/deathPlace'
-							},
-							'predicate_label': {
-								'type': 'literal',
-								'xml:lang': 'en',
-								'value': 'Death place'
-							},
-							'objects': [{
-								'type': 'uri',
-								'value': 'http://dbpedia.org/resource/Las_Vegas',
-								'object_label': {
+							{
+								'predicate': {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/ontology/birthPlace'
+								},
+								'predicate_label': {
 									'type': 'literal',
 									'xml:lang': 'en',
-									'value': 'Las Vegas'
-								}
-							}]
-						}]
+									'value': 'Birth place'
+								},
+								'objects': [{
+									'type': 'uri',
+									'value': 'http://dbpedia.org/resource/New_York_City',
+									'object_label': {
+										'type': 'literal',
+										'xml:lang': 'en',
+										'value': 'New York City'
+									}
+								}]
+							},
+							{
+								'predicate': {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/ontology/deathDate'
+								},
+								'predicate_label': {
+									'type': 'literal',
+									'xml:lang': 'en',
+									'value': 'Death date'
+								},
+								'objects': [{
+									'type': 'typed-literal',
+									'datatype': 'http://www.w3.org/2001/XMLSchema#date',
+									'value': '1996-09-12+02:00'
+								}]
+							},
+							{
+								'predicate': {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/ontology/deathPlace'
+								},
+								'predicate_label': {
+									'type': 'literal',
+									'xml:lang': 'en',
+									'value': 'Death place'
+								},
+								'objects': [{
+									'type': 'uri',
+									'value': 'http://dbpedia.org/resource/Las_Vegas',
+									'object_label': {
+										'type': 'literal',
+										'xml:lang': 'en',
+										'value': 'Las Vegas'
+									}
+								}]
+							}
+						]
 					},
 					{
 						'id': 'artist',
-						'facts': [{
-							'predicate': {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/ontology/occupation'
-							},
-							'predicate_label': {
-								'type': 'literal',
-								'xml:lang': 'en',
-								'value': 'Occupation'
-							},
-							'objects': [{
-								'type': 'uri',
-								'value': 'http://dbpedia.org/resource/Rapping',
-								'object_label': {
+						'facts': [
+							{
+								'predicate': {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/ontology/occupation'
+								},
+								'predicate_label': {
 									'type': 'literal',
 									'xml:lang': 'en',
-									'value': 'Rapping'
-								}
-							}, {
-								'type': 'uri',
-								'value': 'http://dbpedia.org/resource/Actor',
-								'object_label': {
-									'type': 'literal',
-									'xml:lang': 'en',
-									'value': 'Actor'
-								}
-							}]
-						}]
+									'value': 'Occupation'
+								},
+								'objects': [{
+									'type': 'uri',
+									'value': 'http://dbpedia.org/resource/Rapping',
+									'object_label': {
+										'type': 'literal',
+										'xml:lang': 'en',
+										'value': 'Rapping'
+									}
+								}, {
+									'type': 'uri',
+									'value': 'http://dbpedia.org/resource/Actor',
+									'object_label': {
+										'type': 'literal',
+										'xml:lang': 'en',
+										'value': 'Actor'
+									}
+								}]
+							}
+						]
 					}
 				]
 			};
 		});
-
 })();
