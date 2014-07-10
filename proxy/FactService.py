5330040faca0b73349875098e2dcf25dbe334156
Jorge Cruz <jorge@tvplus.com>
2014-07-09 17:32:13 -0700

    FactService: handle cases where resource has no depiction or thumbnail

--- proxy/FactService.py
+++ proxy/FactService.py
@@ -8,16 +8,29 @@ class FactService(object):
     def __init__(self, endpoint = DBpediaEndpoint()):
         self.endpoint = endpoint
 
+    def filter_facts(self, facts, uri):
+        return [node["o"]["value"] for node in facts if node["p"]["value"] == uri]
+
     @coroutine
     def get_resource(self, uri):
         facts = yield self.endpoint.fetch(uri)
 
-        labelNode = [node["o"]["value"] for node in facts if node["p"]["value"] == "http://www.w3.org/2000/01/rdf-schema#label"]
-        depectionNode = [node["o"]["value"] for node in facts if node["p"]["value"] == "http://xmlns.com/foaf/0.1/depiction"]
-        thumbnailNode = [node["o"]["value"] for node in facts if node["p"]["value"] == "http://dbpedia.org/ontology/thumbnail"]
+        labelNode = self.filter_facts(facts, "http://www.w3.org/2000/01/rdf-schema#label")
+        depectionNode = self.filter_facts(facts, "http://xmlns.com/foaf/0.1/depiction")
+        thumbnailNode = self.filter_facts(facts, "http://dbpedia.org/ontology/thumbnail")
+        abstractNode = self.filter_facts(facts, "http://dbpedia.org/ontology/abstract")
+        commentNode = self.filter_facts(facts, "http://www.w3.org/2000/01/rdf-schema#comment")
 
-        return {
+        result = {
             "label": labelNode[0],
-            "depiction": depectionNode[0],
-            "thumbnail": thumbnailNode[0]
+            "abstract": abstractNode[0],
+            "comment": commentNode[0]
         }
+
+        if thumbnailNode:
+            result['thumbnail'] = thumbnailNode[0]
+
+        if depectionNode:
+            result['depiction'] = depectionNode[0]
+
+        return result
--- proxy/FactServiceTest.py
+++ proxy/FactServiceTest.py
@@ -62,7 +62,39 @@ class FactServiceTest(AsyncTestCase):
                 },
                 "predicate_label": {
                     "type": "literal",
-                    "value": "depiction"
+                    "value": "thumbnail"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Abstract"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://dbpedia.org/ontology/abstract"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "has abstract"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Comment"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://www.w3.org/2000/01/rdf-schema#comment"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "comment"
                 }
             }
         ]
@@ -104,6 +136,162 @@ class FactServiceTest(AsyncTestCase):
         expectedLabel = 'http://upload.wikimedia.org/wikipedia/commons/sample_thumbnail.jpg'
         self.assertEqual(expectedLabel, resource['thumbnail'])
 
+
+    @gen_test
+    def test_get_resource_retrieves_the_abstract(self):
+        resourceURI = 'http://dbpedia.org/resource/Sample'
+
+        resource = yield self.fact_service.get_resource(resourceURI)
+        expected = 'Sample Abstract'
+        self.assertEqual(expected, resource['abstract'])
+
+    @gen_test
+    def test_get_resource_retrieves_the_comment(self):
+        resourceURI = 'http://dbpedia.org/resource/Sample'
+
+        resource = yield self.fact_service.get_resource(resourceURI)
+        expected = 'Sample Comment'
+        self.assertEqual(expected, resource['comment'])
+
+
+    @gen_test
+    def test_get_resource_doesnt_retrieve_the_thumbnail_when_missing(self):
+        resourceURI = 'http://dbpedia.org/resource/Sample'
+
+        response = [
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Label"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://www.w3.org/2000/01/rdf-schema#label"
+                },
+                "predicate_label": {
+                    "type": "literal",
+                    "value": "label"
+                }
+            },
+            {
+                "o": {
+                    "type": "uri",
+                    "value": "http://upload.wikimedia.org/wikipedia/commons/sample_depiction.jpg"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://xmlns.com/foaf/0.1/depiction"
+                },
+                "predicate_label": {
+                    "type": "literal",
+                    "value": "depiction"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Abstract"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://dbpedia.org/ontology/abstract"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "has abstract"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Comment"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://www.w3.org/2000/01/rdf-schema#comment"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "comment"
+                }
+            }
+        ]
+
+        future = Future()
+        future.set_result(response)
+
+        self.dbpedia_endpoint.fetch = Mock(return_value=future)
+
+        resource = yield self.fact_service.get_resource(resourceURI)
+        self.assertTrue('thumbnail' not in resource)
+
+
+    @gen_test
+    def test_get_resource_doesnt_retrieve_the_depiction_when_missing(self):
+        resourceURI = 'http://dbpedia.org/resource/Sample'
+
+        response = [
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Label"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://www.w3.org/2000/01/rdf-schema#label"
+                },
+                "predicate_label": {
+                    "type": "literal",
+                    "value": "label"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Abstract"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://dbpedia.org/ontology/abstract"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "has abstract"
+                }
+            },
+            {
+                "o": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "Sample Comment"
+                },
+                "p": {
+                    "type": "uri",
+                    "value": "http://www.w3.org/2000/01/rdf-schema#comment"
+                },
+                "predicate_label": {
+                    "xml: lang": "en",
+                    "type": "literal",
+                    "value": "comment"
+                }
+            }
+        ]
+
+        future = Future()
+        future.set_result(response)
+
+        self.dbpedia_endpoint.fetch = Mock(return_value=future)
+
+        resource = yield self.fact_service.get_resource(resourceURI)
+        self.assertTrue('depiction' not in resource)
         
 
 if __name__ == '__main__':
