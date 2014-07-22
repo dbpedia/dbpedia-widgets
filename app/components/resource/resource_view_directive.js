e04546b19e9f2d8a09d0d9d6c6284836049b379b
Jorge Cruz <jorge@tvplus.com>
2014-07-22 12:37:26 -0700

    adjust comment text

--- app/components/resource/resource_view_directive.js
+++ app/components/resource/resource_view_directive.js
@@ -11,19 +11,20 @@
                 // '<p class="comment">{{ resource.comment }}</p>' + 
                 // '<div class="clear"></div>' + 
 
-                '<div class="media">' +
+                '<div class="media" style="overflow:visible;">' +
                     '<div class="pull-left thumbnail" href="#">' +
                         '<img class="media-object" dbpedia-depiction resource="resource">' +
                     '</div>' +
-                    '<div class="media-body">' +
+                    '<div class="media-body" style="overflow:visible;">' +
                         '<h4 class="media-heading">' +
-                            '<a class="heading" href="{{ resource.wikipedia }}" target="_blank">' + 
-                                '{{ resource.label }}' + 
-                            '</a>' + 
+                            '<a class="heading" href="{{ resource.wikipedia }}" target="_blank">' +
+                                '{{ resource.label }}' +
+                            '</a>' +
                         '</h4>' +
                         '<p class="comment">{{ resource.comment }}</p>' + 
                     '</div>' +
                 '</div>' +
+                '<div class="clear"></div>' +
                 '<ul>' +
                     '<li ng-repeat="groupedFacts in resource.facts">' + 
                         '<dbpedia-grouped-facts facts="groupedFacts">' +
