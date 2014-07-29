2f6d83cd6efef15d824f34684d24c82caf6ecf42
Jorge Cruz <jorge@tvplus.com>
2014-07-29 03:29:54 -0700

    more cleanup

--- app/components/resource/resource_view_directive.js
+++ app/components/resource/resource_view_directive.js
@@ -5,35 +5,25 @@
         return {
             restrict: 'E',
             template: 
-                //'<div class="card" style="height: 100%;">' +
-                    '<div class="resource-depiction">' +
-                        '<img class="media-object img-thumbnail" dbpedia-depiction resource="resource">' +
+                '<div class="resource-depiction">' +
+                    '<img class="media-object img-thumbnail" dbpedia-depiction resource="resource">' +
+                '</div>' +
+                '<div class="resource-header">' +
+                    '<a class="heading" href="{{ resource.wikipedia }}" target="_blank">' +
+                        '{{ resource.label }}' +
+                    '</a>' +
+                    '<p class="comment">{{ resource.comment }}</p>' + 
+                '</div>' +
+                '<div class="grouped-facts-container card-copy">' +
+                    '<div class="generic-facts">' +
+                        '<label class="group-header">About</label>' +
+                        '<p class="content">' +
+                            '{{ resource.comment }}' +
+                        '</p>' +
                     '</div>' +
-                    // '<div class="media-body" style="overflow:visible;">' +
-                    //     '<h4 class="media-heading">' +
-                    //         '<a class="heading" href="{{ resource.wikipedia }}" target="_blank">' +
-                    //             '{{ resource.label }}' +
-                    //         '</a>' +
-                    //     '</h4>' +
-                    //     '<p class="comment">{{ resource.comment }}</p>' + 
-                    // '</div>' +
-                    '<div class="resource-header">' +
-                        '<a class="heading" href="{{ resource.wikipedia }}" target="_blank">' +
-                            '{{ resource.label }}' +
-                        '</a>' +
-                        '<p class="comment">{{ resource.comment }}</p>' + 
-                    '</div>' +
-                    '<div class="grouped-facts-container card-copy">' +
-                        '<div class="generic-facts">' +
-                            '<label class="group-header">About</label>' +
-                            '<p class="content">' +
-                                '{{ resource.comment }}' +
-                            '</p>' +
-                        '</div>' +
-                        '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts">' +
-                        '</dbpedia-grouped-facts>' +
-                    '</div>',// +
-                //'</div>',
+                    '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts">' +
+                    '</dbpedia-grouped-facts>' +
+                '</div>',
             scope: {
                 resource: '='       //two-way parent scope binding
             }
