10cd3ce5351ea97f7df04f13780cfd16e25e56ae
Jorge Cruz <jorge@tvplus.com>
2014-08-13 11:41:15 -0700

    toggle sections

--- app/components/resource/resource_view_directive.js
+++ app/components/resource/resource_view_directive.js
@@ -15,17 +15,69 @@
                     '<p class="comment">{{ resource.comment }}</p>' + 
                 '</div>' +
                 '<div class="grouped-facts-container">' +
-                    '<div class="generic-facts">' +
+                    '<div class="pager">' + 
+                        '<a class="prev" ng-if="hasPrevious()" ng-click="showPrevious()">Prev</a>' + 
+                        '<a class="next" ng-if="hasNext()" ng-click="showNext()">Next</a>' + 
+                    '</div>' +
+                    '<div ng-class="getSectionClasses()" class="generic-facts">' +
                         '<label class="group-header">About</label>' +
                         '<p class="content">' +
                             '{{ resource.comment }}' +
                         '</p>' +
                     '</div>' +
-                    '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts">' +
+                    '<dbpedia-grouped-facts facts="groupedFacts" ng-repeat="groupedFacts in resource.facts" ng-class="getSectionClasses(groupedFacts.id)">' +
                     '</dbpedia-grouped-facts>' +
                 '</div>',
             scope: {
                 resource: '='       //two-way parent scope binding
+            },
+            link: function (scope) {
+                scope.activeSection = 'About';
+
+                scope.hasPrevious = function () {
+                    return scope.activeSection !== 'About';
+                }
+
+                scope.hasNext = function () {
+                    var hasNext = false;
+                    var lastItem;
+                    if (scope.resource.facts.length) {
+                        lastItem = scope.resource.facts[scope.resource.facts.length -1];
+                        hasNext = scope.activeSection !== lastItem.id;
+                    }
+                    return hasNext;
+                };
+
+                scope.showPrevious = function () {
+                    if (scope.activeIndex === 0) {
+                        scope.activeSection = 'About'
+                    }
+                    else
+                    {
+                        scope.activeIndex--;
+                        scope.activeSection = scope.resource.facts[scope.activeIndex].id;
+                    }
+                }
+
+                scope.showNext = function () {
+                    if (scope.activeSection === 'About') {
+                        scope.activeIndex = 0;
+                        scope.activeSection = scope.resource.facts[0].id;
+                    }
+                    else
+                    {
+                        scope.activeIndex++;
+                        scope.activeSection = scope.resource.facts[scope.activeIndex].id;
+                    }
+                }
+
+                scope.getSectionClasses = function (id) {
+                    id = id || 'About';
+
+                    return {
+                        'active-facts': scope.activeSection === id
+                    };
+                }
             }
         };
     }
